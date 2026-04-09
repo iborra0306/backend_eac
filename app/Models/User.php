0e3b4e14953ba8f9b16db6cd67ef14ffd0de2530
Gabriel <2866037@alu.murciaeduca.es>
2026-04-09 13:34:43 +0200

    unidad 2 finalizada

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,11 +9,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
