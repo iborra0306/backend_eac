729295f953efb312ccb13a15e6300cb2a8f7c756
Gabriel <2866037@alu.murciaeduca.es>
2026-04-09 13:48:05 +0200

    3.3 recursos api

---
+++ app/Http/Resources/ModuloCollection.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ModuloCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'data' => $this->collection,
+            'meta' => [
+                'version'   => '1.0',
+                'timestamp' => now()->toIso8601String(),
+                'total'     => $this->total(),
+                'per_page'  => $this->perPage(),
+                'page'      => $this->currentPage(),
+            ],
+        ];
+    }
+}
